a121f51eca6f2138f96689026ab3bb6d219fdb4c
Teniel Smith-Edwards <w5249879@macmini24.gbccloud.com>
2025-02-21 17:10:52 -0500

    completed lab exercise 7

--- LabExercise7/LabExercise7/ViewController.swift
+++ LabExercise7/LabExercise7/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var numText: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ttable: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func generateTable(_ sender: UIButton) {
+        guard let input = numText.text, let num = Int(input) else {return }
+        
+        ttable = (1...10).map {"\(num * $0)"}
+        
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ttable.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "timesCell", for: indexPath)
+        
+        let ans = ttable[indexPath.row]
+        cell.textLabel?.text = ans
+        
+        return cell
+    }
 
 }
 
